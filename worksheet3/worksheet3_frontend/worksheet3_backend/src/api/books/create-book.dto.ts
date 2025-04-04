492d92fdbd44e851ffc40c4dcb6d21023c4335e5
yingzhengwei <wvr8458@autuni.ac.nz>
2025-04-04 14:16:09 +0530

    cleanup

--- worksheet3/worksheet3_frontend/worksheet3_backend/src/api/books/create-book.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateBookDto {
-    title: string;
-    isbn: string;
-    author: string;
-    description: string;
-    published_date: Date;
-    publisher: string;
-    updated_date: Date;
-  }
